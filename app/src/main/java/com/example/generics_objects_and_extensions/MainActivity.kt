040825441c291d80028ad245c6459e821d541153
TheShamRio <xsham@inbox.ru>
2024-11-03 12:12:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.generics_objects_and_extensions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.generics_objects_and_extensions.ui.theme.Generics_objects_and_extensionsTheme
+
+package com.example.generics_objects_and_extensions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.generics_objects_and_extensions.ui.theme.Generics_objects_and_extensionsTheme
+
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+
+fun main() {
+    println("New chat message from a friend")}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Generics_objects_and_extensions"
+include(":app")
+ 
