54c564998e527743a62ad1af7ecf2cc3377e5b3f
TheShamRio <xsham@inbox.ru>
2024-11-03 12:36:39 +0300

    Rewrite extension functions using interfaces

--- app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
+++ app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
@@ -25,7 +25,22 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -37,8 +52,5 @@ class Quiz {
 }
 
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+    Quiz().printProgressBar()
 }
